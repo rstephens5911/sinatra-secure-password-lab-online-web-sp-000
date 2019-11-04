37ed2a6de982bac043bc6d96ef3ef2d480a5e4a7
Ronald Stephens <rstephens@bluemorpho.cc>
2019-11-04 17:27:09 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_users_table.rb
+++ db/migrate/01_create_users_table.rb
@@ -3,7 +3,7 @@ class CreateUsersTable < ActiveRecord::Migration[4.2]
   def change
     create_table :users do |t|
       t.string    :username
-      t.string    :password
+      t.string    :password_digest
       t.float     :balance
     end
   end
